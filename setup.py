addf842d09b827832306f8a43159d5a9e43756c2
Pandu <kottepandurangareddy@gmail.com>
2025-05-13 20:58:46 +0530

    Basic Setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
+
+setup(
+    name = 'Summer Mini Project',
+    packages = find_packages(),
+    version = '0.1.0',
+    description = 'This Machine Learning project focuses on solving real world problem',
+    author = 'Pandu',
+    author_email = 'kottepandurangareddy@gmail.com',
+    install_requires = get_requirements('requirements.txt')
+)
+
